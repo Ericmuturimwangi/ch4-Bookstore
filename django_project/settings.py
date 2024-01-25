0c864f5615df78dbc3a7abae7909cadfbf3a319b
Ericmuturimwangi <muturiwamwanginanyawira@gmail.com>
2024-01-25 19:15:20 +0300

    commit 1.4

--- django_project/settings.py
+++ django_project/settings.py
@@ -111,13 +111,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'acconts.User'
+AUTH_USER_MODEL = 'acconts.CustomUser'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
