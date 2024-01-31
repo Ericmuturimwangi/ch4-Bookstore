97488bd65f2560f5aafbcc529b3c5050a8d1e42a
Ericmuturimwangi <muturiwamwanginanyawira@gmail.com>
2024-01-31 05:06:57 +0300

    2.1

--- django_project/urls.py
+++ django_project/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), 
+    path('login/', LoginView.as_view(), name='login'),
     path('', include('pages.urls')),
     path("books/", include ("books.urls")),
     path ('books/', include ("books.urls")),
